f0d2bdf6715f634953f58152aef0532ffa6b0374
Andrey980 <andreymcunhasilva@gmail.com>
2024-03-17 23:58:13 -0300

    Em andamento

---
+++ contVogais.py
@@ -0,0 +1,23 @@
+# palavra = "abcd"
+
+# for letra in palavra:
+#     print(letra)
+
+
+def contador_vogais(palavra):
+    vogais = 0
+    conso = 0
+
+    for letra in palavra:
+        if letra in "aeiou":
+            vogais += 1
+
+        else:
+            conso += 1
+
+    return conso, vogais
+
+
+valor = input("Digite uma palavra: ")
+
+print("Conso e Vogais: ", contador_vogais(valor))
---
+++ conversorTemp.py
@@ -0,0 +1,23 @@
+def FahToCelsius(valor):
+    return (valor*9/5) + 32
+
+
+def CelsiusToFah(valor):
+    return (valor - 32) * 5/9
+
+
+opcao = input(
+    "Digite qual conversão deseja: Celsius -> Fahrenheit (F) / Fahrenheit -> Celsius (C) ").upper()
+
+if opcao == 'F':
+    celsius = float(input("Digite a temperatura em Celsius: "))
+    resultado = CelsiusToFah(celsius)
+    print("A temperatura em Fahrenheit é:", resultado)
+
+elif opcao == 'C':
+    fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
+    resultado = FahToCelsius(fahrenheit)
+    print("A temperatura em Fahrenheit é:", resultado)
+
+else:
+    print("Opção inválida.")
